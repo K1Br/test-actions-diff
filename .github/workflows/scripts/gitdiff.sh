f71b2a53b11050ed467c15669633a6612eb9a9e3
Kim <22888464+K1Br@users.noreply.github.com>
2021-08-20 16:01:31 +0100

    Update gitdiff.sh

--- .github/workflows/scripts/gitdiff.sh
+++ .github/workflows/scripts/gitdiff.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+set -e
 git diff --diff-filter=ACMRT ${{ github.event.pull_request.base.sha }} ${{ github.sha }} -U0 | grep 'review_date:'| cut -d\  -f2 >> date_diff.csv
 cat date_diff.csv
 echo "GITHUB_OLD_DATE=$(echo $(head -1 date_diff.csv))" >> $GITHUB_ENV
