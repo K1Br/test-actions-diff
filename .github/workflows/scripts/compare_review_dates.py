9539c23f18eae713069c80ab26b8b6400d66fdca
k1br <22888464+K1Br@users.noreply.github>
2021-09-17 15:25:21 +0100

    testing if read works

--- .github/workflows/scripts/compare_review_dates.py
+++ .github/workflows/scripts/compare_review_dates.py
@@ -3,10 +3,11 @@ import os
 import pandas as pd
 test = pd.read_csv('gitdiff_dates.csv')
 print(test)
-print(os.getenv('GITHUB_OLD_DATE'),"old date")
+'''print(os.getenv('GITHUB_OLD_DATE'),"old date")
 print(os.getenv('GITHUB_NEW_DATE'), "new date")
 
 if (os.environ['GITHUB_NEW_DATE'] != ''):
     date_diff = dt.strptime(os.environ['GITHUB_NEW_DATE'], "%Y-%m-%d") - dt.strptime(os.environ['GITHUB_OLD_DATE'], "%Y-%m-%d")
     print(date_diff)
     if date_diff.days > 92: raise ValueError("date diff to high")
+'''
