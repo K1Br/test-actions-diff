564905a43ffa9769f41ffb8cdfc766202c5f0b6b
k1br <22888464+K1Br@users.noreply.github>
2021-09-17 15:35:02 +0100

    testing if read works

--- .github/workflows/scripts/compare_review_dates.py
+++ .github/workflows/scripts/compare_review_dates.py
@@ -1,8 +1,25 @@
 from datetime import datetime as dt
-import os
+from pathlib import Path
 import pandas as pd
-test = pd.read_csv('gitdiff_dates.csv')
-print(test)
+
+#Read as text blob
+txt = Path('gitdiff_dates.csv').read_text()
+
+#Split the files by where it begins to report a new file
+updated_files = txt.split('diff --git')
+
+# Retain only updated files that have review dates altered and extract the dates
+reviewed_files = [i for i in updated_files if '-review_date' in i]
+match = re.findall(r'\d{4}-\d{2}-\d{2}', reviewed_files[0])
+
+#compare if the review dates are within our specified bounds
+for a, b in itertools.combinations(match, 2):
+    date_diff = dt.strptime(b, "%Y-%m-%d") - dt.strptime(a, "%Y-%m-%d")
+    print(date_diff)
+    if date_diff.days > 92: raise ValueError("date diff to high")
+
+
+
 '''print(os.getenv('GITHUB_OLD_DATE'),"old date")
 print(os.getenv('GITHUB_NEW_DATE'), "new date")
 
