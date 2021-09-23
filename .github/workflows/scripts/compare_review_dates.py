4d36db568f8695844506a8547acbed69dc14d2bb
k1br <22888464+K1Br@users.noreply.github>
2021-09-23 14:54:26 +0100

    adding error statement in traceback

--- .github/workflows/scripts/compare_review_dates.py
+++ .github/workflows/scripts/compare_review_dates.py
@@ -20,5 +20,5 @@ for outtext in reviewed_files:
     for a, b in itertools.combinations(match, 2):
         date_diff = dt.strptime(b, "%Y-%m-%d") - dt.strptime(a, "%Y-%m-%d")
         if date_diff.days > 92:
-            error_statement = "Date difference too high: " + str(date_diff.days) + " in file: " + filename + "Needs to be under X months/ X days" + " Dates: " + *match
+            error_statement = "Date difference too high: " + str(date_diff.days) + " in file: " + filename + "Needs to be under X months/ X days" + " Dates: " + match
             raise ValueError("Date diff too high", error_statement)
