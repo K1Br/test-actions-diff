2b206353a1544c534b8d5610c82e73409ee5461e
k1br <22888464+K1Br@users.noreply.github>
2021-09-23 14:21:17 +0100

    adding filename in traceback

--- .github/workflows/scripts/compare_review_dates.py
+++ .github/workflows/scripts/compare_review_dates.py
@@ -16,8 +16,9 @@ reviewed_files = [i for i in updated_files if '-review_date' in i]
 #compare if the review dates are within our specified bounds
 for outtext in reviewed_files:
     match = re.findall(r'\d{4}-\d{2}-\d{2}', outtext)
+    filename= re.findall(r'(?<=a\/)(.*)(?!.yml)',outtext)
     for a, b in itertools.combinations(match, 2):
         date_diff = dt.strptime(b, "%Y-%m-%d") - dt.strptime(a, "%Y-%m-%d")
         if date_diff.days > 92:
-            print("Date difference too high: ",date_diff.days," Needs to be under X months/ X days", " Dates: ",match)
+            print("Date difference too high: ",date_diff.days," in file: ", filename, Needs to be under X months/ X days", " Dates: ",match)
             raise ValueError("Date diff too high")
